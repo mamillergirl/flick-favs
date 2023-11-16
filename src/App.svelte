5b08de00e1027a609278e4f1ddf8b8c8c28173de
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-11-16 10:20:55 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Search from "./lib/Search.svelte";
+</script>
+
+<main>
+  <Search/>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Search.svelte
@@ -0,0 +1,25 @@
+<script>
+    let searchWord = "";
+    let movieData = [];
+    async function search(){
+        let url = `https://www.omdbapi.com/?apikey=3ca7a7ca&t=${searchWord}`
+        let data = await fetch(url);
+        let response = await data.json();
+        movieData = [response];
+    }
+</script>
+
+<fieldset>
+    <input bind:value={searchWord}>
+    <button on:click={search}>Search</button>
+</fieldset>
+
+{#each movieData as movie}
+    <div class="movie-card">
+        <h2>{movie.Title}</h2>
+        <img alt = {movie.Title} src={movie.Poster}>
+        <p>Released: {movie.Released}</p>
+        <p>Director: {movie.Director}</p>
+     
+    </div>
+{/each}
