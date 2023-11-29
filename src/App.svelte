593bc4b81630331596571180914ff8ee93098474
Julia Nye <jujaritchie@gmail.com>
2023-11-29 11:17:09 -0700

    Finished Main Content On Homepage Worked on the Our Mission and Our Story section along with adding the Movie Watchlist button.

--- src/App.svelte
+++ src/App.svelte
@@ -51,6 +51,17 @@
     <Search/>
   {:else}
     <h2>Welcome Home</h2>
+    <div class="purpose-box">
+      <div class="mission">
+        <h2>Our Mission</h2>
+        <p>Our mission is to bring you the movies you are looking for. Oftentimes, many people will spend hours scrolling through movies and not being able to decide which movies to sit down and watch. Our interactive website allows you to search for movies and get a brief description that can help you choose a movie.</p>
+        </div>
+        <div class="story">
+          <h2>Our Story</h2>
+          <p>We are college students, tired of not being able to find movies to watch because of too many options. Together, we created Flick Favs as a way to find movie information and save interesting movies to watchlists. We also implemented a way for users to comment about movies they like or dislike for the benefit of others.</p>
+        </div>
+    </div>
+    <a href="#search"><button>Create Your Movie Wishlist!</button></a>
   {/if}
 </main>
 
