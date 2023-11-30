a7821dc476468a3440895ae42b0b6c0bcd1ae0f7
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-11-30 11:13:10 -0700

    add watchlist and add and remove buttons

---
+++ src/lib/Watchlist.svelte
@@ -0,0 +1,13 @@
+<script>
+    import MovieCard from "./MovieCard.svelte";
+    import { getLocalStorage } from './stores.mjs';
+
+    let movieData = getLocalStorage('watchlist');
+
+</script>
+
+{#each movieData as movie}
+    {#if movie.Type != "game"}
+    <MovieCard {movie}/>
+   {/if}
+{/each}
